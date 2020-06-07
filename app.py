586dea89230a62beba848c3c232f52d3ae0afaf8
Matt Sunner <sunner.matt@gmail.com>
2020-06-07 18:23:08 -0500

    Format script to create new xlsx file for each line in txt file

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
-from openpyxl import Workbook
+"""
+Sales Log Generator
 
+Author: Matthew Sunner, 2020
+"""
+
+# Imports
+from openpyxl import Workbook
 
 # Import sample list
 sample_txt = open('sample.txt', 'r')
@@ -11,6 +17,9 @@ for line in sample_txt:
     sales_items.append(line.strip())
 
 # Iterate through the array and create a new, standardized workbook for each item
-
+for item in sales_items:
+    wb = Workbook()
+    wb.save(f'workbooks/Sales-Log-{item}.xlsx')
 
 # Tear down & close
+wb.close()
