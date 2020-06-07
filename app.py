3beacc257714d6b24b641afbc51c7b344d1f76d6
Matt Sunner <sunner.matt@gmail.com>
2020-06-07 18:55:02 -0500

    Add in function for templating workbooks; refactor code to functional state

--- app.py
+++ app.py
@@ -5,21 +5,26 @@ Author: Matthew Sunner, 2020
 """
 
 # Imports
-from openpyxl import Workbook
+from openpyxl import Workbook, load_workbook
 
-# Import sample list
-sample_txt = open('sample.txt', 'r')
 
-# Iterate through list and add each item to an array
-sales_items = []
+def salesLogging():
+    # Import sample list
+    sample_txt = open('sample.txt', 'r')
 
-for line in sample_txt:
-    sales_items.append(line.strip())
+    # Iterate through list and add each item to an array
+    sales_items = []
 
-# Iterate through the array and create a new, standardized workbook for each item
-for item in sales_items:
-    wb = Workbook()
-    wb.save(f'workbooks/Sales-Log-{item}.xlsx')
+    for line in sample_txt:
+        sales_items.append(line.strip())
 
-# Tear down & close
-wb.close()
+    # Iterate through the array and create a new, standardized workbook for each item
+    for item in sales_items:
+        wb = load_workbook('original.xlsx')
+        ws = wb.active
+        ws['B3'] = item
+        wb.save(f'workbooks/Sales-Log-{item}.xlsx')
+        wb.close()
+
+if __name__ == '__main__':
+    salesLogging()
